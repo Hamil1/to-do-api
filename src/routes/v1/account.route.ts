ce19a956bbc5dd03e1c307d3e2388678f229f256
Victor H. Diaz <victor.diaz@optic.gob.do>
2021-06-30 21:13:16 -0400

    fix(routes): changing the register endpoint
    changing the register endpoint from /register to /v1/account/register

--- src/routes/v1/account.route.ts
+++ src/routes/v1/account.route.ts
@@ -5,12 +5,12 @@ import auth from '../../middlewares/auth.middleware';
 const accountRouter = Router();
 const accountController = new AccountController();
 
-accountRouter.get('/', accountController.get);
-accountRouter.post('/', accountController.register);
-
 //applying the auth middleware to all below routes
 accountRouter.get('*', auth);
 accountRouter.delete('*', auth);
+
+accountRouter.get('/', accountController.get);
+accountRouter.post('/register', accountController.register);
 
 accountRouter.post('/login', accountController.login);
 accountRouter.get('/:id', accountController.getById);
